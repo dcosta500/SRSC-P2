8728efc33576a94bfbd7c6819cbdde1a78890825
Diogo Costa <diogo.alex.nc@gmail.com>
2023-11-24 02:21:45 +0000

    Uncommented genNewCerts line in compilation script

--- compileProject.sh
+++ compileProject.sh
@@ -11,6 +11,6 @@ javac -d out ./src/utils/* ./src/client/*
 javac -d out ./src/utils/* ./src/servers/MainDispatcher/*
 javac -d out ./src/utils/* ./src/servers/AuthenticationServer/*
 
-# genNewCerts
+genNewCerts
 
 echo "Project Compiled."
