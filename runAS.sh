ca9e54c6a5174825075ebfbb1e3c9f974ed9083c
Tomé Dias <td.dias@campus.fct.unl.pt>
2023-12-01 14:57:04 +0000

    Fixed .sh (Did an oopsie)

---
+++ runAS.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp out:src/jar/sqlite-jdbc-3.44.0.0.jar:src/jar/slf4j-api-1.7.36.jar servers.AuthenticationServer.ASServer
---
+++ runC.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp out client.Client $1
