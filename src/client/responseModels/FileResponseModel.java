ab06348eb214471ea9bfda1e780f5ded42976eea
Diogo Costa <diogo.alex.nc@gmail.com>
2023-12-04 17:18:45 +0000

    Changes by Vasco

---
+++ src/client/responseModels/FileResponseModel.java
@@ -0,0 +1,15 @@
+package client.responseModels;
+
+public class FileResponseModel {
+
+        private String response;
+
+        public FileResponseModel(String response) {
+            this.response = response;
+        }
+
+        public String getResponse() {
+            return response;
+        }
+}
+
