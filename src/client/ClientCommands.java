dac81df3896b7c0ae899d79334be435fa75aa936
Diogo Costa <diogo.alex.nc@gmail.com>
2023-12-04 00:15:02 +0000

    Changes by Vasco

--- src/client/ClientCommands.java
+++ src/client/ClientCommands.java
@@ -268,7 +268,6 @@ public abstract class ClientCommands {
         }
 
         byte[] content = CryptoStuff.symDecrypt(client_auth_key, rp.getContent());
-        
 
         //  Kvtoken = { len + { len + kvtoken_content || len + SIGac( kvtoken_content ) } Kac,s }
         //  kvtoken_content = { len + uid || len + IpClient || len + IdService || len + TSi || len + TSf || len + Kc,s  || len + perms }
